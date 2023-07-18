e2507f26a28a4f74fc161e8e52b64e645df2e65e
Catraco <gpx.zero.000@gmail.com>
2023-07-18 15:12:31 +0100

    Add navigator visit count

--- pages/api/visit.js
+++ pages/api/visit.js
@@ -4,11 +4,18 @@ const fs = require('fs')
 const fileName = 'visit.json'
 
 export default function handler(req, res) {
-    saveCount()
-    res.status(200).json({ status: 200 })
+    if (req.query.path === 'admin') getCount(req, res)
+    else saveCount(req, res)
 }
 
-function saveCount() {
+function getCount(req, res) {
+    fs.readFile(fileName, (err, data) => {
+        if (err) throw err
+        res.status(200).json(data ? JSON.parse(data) : {})
+    })
+}
+
+function saveCount(req, res) {
     const date = new Date().toDateString()
     let visit = {}
     visit[date] = 1
@@ -16,7 +23,7 @@ function saveCount() {
         fs.readFile(fileName, (err, data) => {
             if (err) throw err
             visit = JSON.parse(data)
-            if(visit[date]) visit[date]++
+            if (visit[date]) visit[date]++
             else visit[date] = 1
             fs.writeFileSync(fileName, JSON.stringify(visit), function (err) {
                 if (err) throw err
@@ -27,4 +34,5 @@ function saveCount() {
             if (err) throw err
         })
     }
+    res.status(200).json({ status: 200 })
 }
